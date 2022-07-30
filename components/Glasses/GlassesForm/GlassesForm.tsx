d11eaa52fc0a6f447486abfc2703b93692376328
msakac <msakac@foi.hr>
2022-07-30 14:57:44 +0200

    feat(Button): added button component

--- components/Glasses/GlassesForm/GlassesForm.tsx
+++ components/Glasses/GlassesForm/GlassesForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button';
 import Input from '../../UI/Input';
 
 export default function GlassesForm() {
@@ -11,11 +12,8 @@ export default function GlassesForm() {
         <Input id={'price'} title={'Price'} type="number" min="1" step="any" required placeholder="$" />
       </div>
       <Input id={'description'} title={'Description'} type="textarea" required />
-      <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Purple to blue
-        </span>
-      </button>
+      <Button title={'Cancel'} style={'mr-[20px]'}/>
+      <Button title={'Add Glasses'} withBorder={false} />
     </form>
   );
 }
