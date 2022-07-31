bfe7e9325fc73d1dc5c85fd6271dac4d22151a72
msakac <msakac@foi.hr>
2022-07-31 14:19:39 +0200

    Added image input to glasses form

--- components/Glasses/GlassesForm/GlassesForm.tsx
+++ components/Glasses/GlassesForm/GlassesForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../../UI/Button';
 import Input from '../../UI/Input';
+import TextArea from '../../UI/TextArea';
 
 interface IGlassesFormProps{
   onCancel: () => void
@@ -13,9 +14,10 @@ export default function GlassesForm({onCancel} : IGlassesFormProps) {
         <Input id={'glasses_name'} title={'Glasses name'} required placeholder="Some Glasses" />
         <Input id={'type'} title={'Type'} placeholder="Some Type" required />
         <Input id={'qty'} title={'Quantity'} type="number" min="1" max="100" required placeholder="xxx" />
-        <Input id={'price'} title={'Price'} type="number" min="1" step="any" required placeholder="$" />
+        <Input id={'price'} title={'Price'} type="number" min="1" step="any" required placeholder="99.99$" />
       </div>
-      <Input id={'description'} title={'Description'} type="textarea" required />
+      <TextArea id={'description'} title={'Description'} type="textarea" required rows='5' placeholder="Description.." />
+      <Input id={'image'} title={'Image'} type="text" min="1" required placeholder="http://image.com" />
       <Button title={'Cancel'} onClick={onCancel} style={'mr-[20px]'}/>
       <Button title={'Add Glasses'} withBorder={false} />
     </form>
