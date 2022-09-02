6d8424dc44d5d4d49c0e6ed7060b1350d354f199
Martin Sakac <msakac@foi.hr>
2022-09-02 14:13:14 +0200

    Fixing and breaking stuff

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -8,35 +8,37 @@ import { GradientColors } from '../../common/types';
 
 export default function LoginForm() {
   return (
-    <section className="w-[80%] h-[100%] m-auto my-6 border dark:border-gray-600 rounded-2xl flex flex-col p-5 justify-center items-center gap-3 bg-gray-50 dark:bg-gray-800 md:w-[60%] lg:w-[40%]">
-      <VectorLogin className="w-[100%] h-[100%] my-[-100px] max-w-[500px]" />
-      <h1 className="self-start text-4xl text-white font-bold border-b-2 w-full text-start pb-2 mb-6 border-cyan-500">Login</h1>
-      <form className="flex flex-col gap-4 self-stretch">
-        <div className="flex gap-3">
-          <VectorEnvelope className="w-[30px] h-[30px] fill-white self-end" />
+    <section className="w-[90%] h-[100%] m-auto my-6 border dark:border-gray-600 rounded-2xl flex flex-col p-5 justify-center items-center gap-3 bg-gray-50 dark:bg-gray-800 md:w-[60%] lg:w-[40%]">
+      <VectorLogin className="w-[90%] h-[90%] my-[-100px] max-w-[350px] md:max-w-[500px]" />
+      <h1 className="self-start text-3xl text-white font-bold border-b-2 w-full text-start pb-2 mb-6 border-cyan-500">Login</h1>
+      <form className="flex flex-col gap-4 self-stretch" autoComplete="off">
+        <div className="flex gap-3 group">
+          <VectorEnvelope className="w-[40px] h-[40px] fill-white self-end group-focus-within:fill-cyan-400 group-focus-within:animate-pulse group-focus-within:transition-colors" />
           <input
-            type="text"
+            type="email"
             name="email"
             id="email"
+            autoComplete="new-password"
             placeholder="E-mail"
-            className="w-[100%] bg-transparent ml-3 border-solid border-b-[1px] border-gray-500 text-white py-2 focus:outline-none focus:border-b-2 focus:border-cyan-500 focus:animate-pulse transition-colors"
+            className="w-[100%] bg-transparent ml-3 border-solid border-b-[1px] border-gray-500 text-white py-2 focus:outline-none focus:border-b-2 focus:border-cyan-500"
           />
         </div>
-        <div className="flex gap-3 self-stretch">
-          <VectorText className="w-[30px] h-[30px] fill-white self-end" />
+        <div className="group flex gap-3 self-stretch">
+          <VectorText className="w-[40px] h-[40px] fill-white self-end group-focus-within:fill-cyan-400 group-focus-within:animate-pulse group-focus-within:transition-colors" />
           <input
-            type="text"
+            type="password"
             name="email"
             id="email"
             placeholder="Password"
-            className="w-[100%] ml-3 bg-transparent border-solid border-b-[1px] border-gray-500 text-white py-2 focus:outline-none focus:border-b-2 focus:border-cyan-500 focus:animate-pulse transition-colors"
+            autoComplete="new-password"
+            className="w-[100%] ml-3 bg-transparent border-solid border-b-[1px] border-gray-500 text-white py-2 focus:outline-none focus:border-b-2 focus:border-cyan-500"
           />
         </div>
       </form>
       <div className="mt-4 flex justify-between w-full">
         <div className="text-white">
           <input type="checkbox" id="remember" />
-          <label htmlFor="remember" className="mx-2 text-gray-400 font-semibold">
+          <label htmlFor="remember" className="mx-2 text-gray-400 font-semibold text-sm">
             Remember me
           </label>
         </div>
@@ -47,8 +49,15 @@ export default function LoginForm() {
       <Button
         title={'Login'}
         withBorder={false}
-        style={`w-full ${GradientColors.CYAN_TO_BLUE} hover:outline hover:outline-offset-2 hover:outline-1`}
+        style={`w-[90%] rounded-full ${GradientColors.CYAN_TO_BLUE} hover:outline hover:outline-offset-2 hover:outline-1`}
       />
+      <p className="or-login">OR</p>
+      <div className="flex">
+        <p className="text-sm text-gray-500 font-semibold">New to Gleseso?</p>
+        <Link href="/">
+          <a className="text-sm text-cyan-400 font-semibold ml-1 hover:border-b-[1px] hover:border-cyan-400 hover:mt-[-1px]">Register</a>
+        </Link>
+      </div>
     </section>
   );
 }
--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,5 +1,4 @@
-import React, { ReactElement } from 'react';
-
+import React from 'react';
 interface IButtonProps {
   title: string;
   onClick?: () => void;
@@ -8,7 +7,6 @@ interface IButtonProps {
 }
 
 export default function Button({ title, onClick, style = '', withBorder = true }: IButtonProps) {
-  console.log(style);
   return (
     <button onClick={onClick} className={classes.button(style)}>
       <span className={classes.span(withBorder)}>{title}</span>
@@ -18,12 +16,10 @@ export default function Button({ title, onClick, style = '', withBorder = true }
 
 const classes = {
   button: (style: string) => `my-5 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden 
-  text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 
+  text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br bg-gradient-to-br from-cyan-400 to-blue-500 
   group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white 
   focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ${style}`,
 
-  span: (withBorder: boolean) => `relative px-5 py-2.5 transition-all ease-in duration-75 ${
-    withBorder && 'bg-white dark:bg-gray-900'
-  } 
+  span: (withBorder: boolean) => `relative px-5 py-2.5 transition-all ease-in duration-75 ${withBorder && 'bg-white dark:bg-gray-900'} 
   rounded-md group-hover:bg-opacity-0 text-md text-lg font-semibold`
 };
